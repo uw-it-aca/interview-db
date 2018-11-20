3865c8ff63b5247ec293c53b2b959000f53d037f
Jason Civjan <jcivjan@uw.edu>
2018-11-20 13:20:08 -0800

    rearranging templates. Adding dynamic template for interview instance.

--- interview_db/views.py
+++ interview_db/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
-
 from .models import Code, Interview
 
 
@@ -11,8 +10,8 @@ class IndexView(ListView):
         return Code.objects.order_by('code')
         
 
-class InterviewView(ListView):
-    template_name = 'interview.html'
+class InterviewsView(ListView):
+    template_name = 'interviews.html'
     context_object_name = 'interview_list'
     def get_queryset(self):
         return Interview.objects.order_by('student')
@@ -22,3 +21,15 @@ class PeopleView(ListView):
     context_object_name = 'interview_list'
     def get_queryset(self):
         return Interview.objects.order_by('student')
+
+class TopicsView(ListView):
+    template_name = 'topics.html'
+    context_object_name = 'code_list'
+    def get_queryset(self):
+        return Code.objects.order_by('code')
+        
+def interview(request, interview_id):
+    interview = get_object_or_404(Interview, pk=interview_id)
+    return render(request, 'interview.html', {
+        'interview': interview
+        })
