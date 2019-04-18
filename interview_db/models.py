4695991a14c5f9d81ea78b28f3303702b4af150b
Ethan Turner <ezturner@washington.edu>
2019-04-18 13:18:52 -0700

    Fixing indentation and mixed tabs/spaces

--- interview_db/models.py
+++ interview_db/models.py
@@ -13,17 +13,18 @@ class StudentType(models.Model):
         ('International', 'International'),
         ('LGBTQ', 'LGBTQ'),
         ('Transfer', 'Transfer'),
-    ) 
+    )
 
     type = models.CharField(choices=TYPE,
-                               max_length=100,
-                               blank=True,
-                               null=True
-                               )
+                            max_length=100,
+                            blank=True,
+                            null=True
+                            )
 
     def __str__(self):
         return self.type
-        
+
+
 class Major(models.Model):
 
     full_title = models.CharField(max_length=255)
@@ -31,52 +32,53 @@ class Major(models.Model):
 
     def __str__(self):
         return self.major_abbreviation
-    
+
     class Meta:
-        ordering = ['major_abbreviation',]
-        
+        ordering = ['major_abbreviation', ]
+
+
 class Location(models.Model):
 
     location = models.CharField(max_length=255,
-                               )
+                                )
 
     def __str__(self):
         return self.location
-    
-    class Meta:
-        ordering = ['location',]
 
-        
-class Student(models.Model):  
+    class Meta:
+        ordering = ['location', ]
+
+
+class Student(models.Model):
 
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     uw_netid = models.CharField(max_length=10, blank=True, null=True)
     email = models.EmailField(max_length=255,
-                                blank=True,
-                                null=True)
-    artifacts_url = models.URLField(help_text="URL for Google Drive folder where student artifacts are stored.")
-    follow_up_consent = models.BooleanField()    
-                                     
-                                 
+                              blank=True,
+                              null=True)
+    artifacts_url = models.URLField(
+        help_text="URL for Google Drive folder where student artifacts are stored.")
+    follow_up_consent = models.BooleanField()
+
     def __unicode__(self):
         return unicode(self.first_name) + " " + unicode(self.last_name)
-    
-    class Meta:
-        ordering = ['first_name',]
 
-               
+    class Meta:
+        ordering = ['first_name', ]
+
+
 class Interview(models.Model):
-	YEAR_LEFT = (
+    YEAR_LEFT = (
         ('< 1', '< 1'),
         ('1', '1'),
         ('2', '2'),
         ('3', '3'),
         ('4', '4'),
         ('5', '5'),
-        ('6', '6')      
+        ('6', '6')
     )
-	YEAR = (
+    YEAR = (
         ('1', '1'),
         ('2', '2'),
         ('3', '3'),
@@ -84,66 +86,74 @@ class Interview(models.Model):
         ('5', '5'),
         ('6', '6'),
         ('7', '7'),
-        ('8', '8')        
+        ('8', '8')
     )
-	STANDING = (
+    STANDING = (
         ('Fr', 'Freshman'),
         ('So', 'Sophmore'),
         ('Jr', 'Junior'),
         ('Sr', 'Senior'),
         ('Al', 'Alumni - undergrad'),
         ('Ma', 'Masters'),
-        ('Ph', 'PhD')       
+        ('Ph', 'PhD')
     )
-    	QUARTER = (
-		('au', 'Autumn'),
-		('wi', 'Winter'),
-		('sp', 'Spring'),
-		('su', 'Summer')
+    QUARTER = (
+        ('au', 'Autumn'),
+        ('wi', 'Winter'),
+        ('sp', 'Spring'),
+        ('su', 'Summer')
     )
-    
-	student = models.ForeignKey(Student,on_delete=models.PROTECT)
-	date= models.DateField()
-	interview_quarter = models.CharField(choices=QUARTER,max_length=2)
-	interview_location = models.ManyToManyField(Location,
-                                           blank=True)
-	signed_release_form = models.BooleanField()
-	pull_quote = models.TextField(max_length=200, help_text="A brief, 200 character or less, quote from interview that will engage people.")
-	interview_notes_url = models.URLField(blank=True,
-                                  null=True,
-                                  help_text="Direct URL for transcribed interview, edited for publication.")
-	image = models.ImageField(upload_to='interview_db_images', default="", blank=True, null=True, help_text="Image should be 1875 x 2882 (any orientation), saved as an optimized High Quality (60) JPEG.")
-	image_is_not_identifying = models.BooleanField(help_text="This image doesn't include the student's face.")
-	image_alt_text = models.CharField(max_length=255, blank=True, null=True, help_text="Describe the image in detail so that a non-sighted user might also get that personal connection.")
-	intended_major = models.BooleanField(blank=True)
-	major = models.ManyToManyField(Major) 
-	standing = models.CharField(choices=STANDING,
-                               max_length=2,
-                               blank=True)
-	years_until_graduation = models.CharField(choices=YEAR_LEFT,
-                               max_length=3,
-                               blank=True)
-	current_year = models.CharField(choices=YEAR,
-                               max_length=1,
-                               blank=True)
-	student_type = models.ManyToManyField(StudentType,
-                                           blank=True)   
-	no_identifying_photo = models.BooleanField(help_text="No photo of face can be published.")
-	no_real_name = models.BooleanField(help_text="Cannot publish real name.")
-	no_publishing_stories = models.BooleanField(help_text="Stories and artifacts can only be used internally for research.")
-	other_publishing_restrictions = models.BooleanField(help_text="They had other restrictions on the use of their data.")
-	other_publishing_restrictions_notes = models.TextField(blank=True,null=True)
-	
-	def __str__(self):
-            return str(self.student)+ ": " +str(self.date)
-        class Meta:
-            ordering = ['student','-date']
-        
-	def declared_major(self):
-    	    return ','.join([ major.major_abbreviation for major in self.major.all() ])
 
-	def get_absolute_url(self):
-            return reverse('interview_db:interview', args=(), kwargs={'interview_id': str(self.id)})   
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    date = models.DateField()
+    interview_quarter = models.CharField(choices=QUARTER, max_length=2)
+    interview_location = models.ManyToManyField(Location,
+                                                blank=True)
+    signed_release_form = models.BooleanField()
+    pull_quote = models.TextField(
+        max_length=200, help_text="A brief, 200 character or less, quote from interview that will engage people.")
+    interview_notes_url = models.URLField(blank=True,
+                                          null=True,
+                                          help_text="Direct URL for transcribed interview, edited for publication.")
+    image = models.ImageField(upload_to='interview_db_images', default="", blank=True, null=True,
+                              help_text="Image should be 1875 x 2882 (any orientation), saved as an optimized High Quality (60) JPEG.")
+    image_is_not_identifying = models.BooleanField(
+        help_text="This image doesn't include the student's face.")
+    image_alt_text = models.CharField(max_length=255, blank=True, null=True,
+                                      help_text="Describe the image in detail so that a non-sighted user might also get that personal connection.")
+    intended_major = models.BooleanField(blank=True)
+    major = models.ManyToManyField(Major)
+    standing = models.CharField(choices=STANDING,
+                                max_length=2,
+                                blank=True)
+    years_until_graduation = models.CharField(choices=YEAR_LEFT,
+                                              max_length=3,
+                                              blank=True)
+    current_year = models.CharField(choices=YEAR,
+                                    max_length=1,
+                                    blank=True)
+    student_type = models.ManyToManyField(StudentType,
+                                          blank=True)
+    no_identifying_photo = models.BooleanField(
+        help_text="No photo of face can be published.")
+    no_real_name = models.BooleanField(help_text="Cannot publish real name.")
+    no_publishing_stories = models.BooleanField(
+        help_text="Stories and artifacts can only be used internally for research.")
+    other_publishing_restrictions = models.BooleanField(
+        help_text="They had other restrictions on the use of their data.")
+    other_publishing_restrictions_notes = models.TextField(
+        blank=True, null=True)
+
+    def __str__(self):
+        return str(self.student) + ": " + str(self.date)
+    class Meta:
+        ordering = ['student','-date']
+        
+    def declared_major(self):
+        return ','.join([ major.major_abbreviation for major in self.major.all() ])
+
+    def get_absolute_url(self):
+        return reverse('interview_db:interview', args=(), kwargs={'interview_id': str(self.id)})   
 
 class ResourceCategory(models.Model):
     resource_category = models.CharField(max_length=500)
