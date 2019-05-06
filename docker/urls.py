6261e512cea763086c45773499197f5c77224f8e
Ethan Turner <ezturner@washington.edu>
2019-05-06 15:49:13 -0700

    Adding interview_db urls

--- docker/urls.py
+++ docker/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path 
+from django.urls import path, re_path, include
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^', include('interview_db.urls')),
 ]
