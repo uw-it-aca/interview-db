e5271616a8fa679343f651dab7a5e59fa50731b6
Jason Civjan <jasoncivjan@Unknown.domain>
2019-08-19 16:49:34 -0700

    fixing docker urls

--- docker/urls.py
+++ docker/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, re_path, include
 from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    url('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     re_path(r'^', include('interview_db.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
