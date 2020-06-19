daf5f15337e1db5036b9c5c920994840db91d09d
Sarthak Singh <ss269@uw.edu>
2020-06-18 18:35:09 -0700

    Switched how login required is done

--- interview_db/views.py
+++ interview_db/views.py
@@ -1,39 +1,50 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from .models import Code, Interview
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-class IndexView(LoginRequiredMixin, ListView):
+@method_decorator(login_required(), name='dispatch')
+class IndexView(ListView):
     template_name = 'index.html'
     context_object_name = 'interview_list'
     def get_queryset(self):
         return Interview.objects.order_by('student')
-        
 
-class InterviewsView(LoginRequiredMixin, ListView):
+
+@method_decorator(login_required(), name='dispatch')
+class InterviewsView(ListView):
     template_name = 'interviews.html'
     context_object_name = 'interview_list'
     def get_queryset(self):
         return Interview.objects.order_by('student')
-        
-class PeopleView(LoginRequiredMixin, ListView):
+
+
+@method_decorator(login_required(), name='dispatch')
+class PeopleView(ListView):
     template_name = 'people.html'
     context_object_name = 'interview_list'
     def get_queryset(self):
         return Interview.objects.order_by('student')
 
-class TopicsView(LoginRequiredMixin, ListView):
+
+@method_decorator(login_required(), name='dispatch')
+class TopicsView(ListView):
     template_name = 'topics.html'
     context_object_name = 'code_list'
     def get_queryset(self):
         return Code.objects.order_by('topic','code')
-        
+
+
+@login_required()
 def interview(request, interview_id):
     interview = get_object_or_404(Interview, pk=interview_id)
     return render(request, 'interview.html', {
         'interview': interview
         })
 
+
+@login_required()
 def code(request, code_id):
     code = get_object_or_404(Code, pk=code_id)
     return render(request, 'topic.html', {
