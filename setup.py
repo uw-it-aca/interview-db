796e1b77e07b6dea684262e5f54796006844241b
Sarthak Singh <ss269@uw.edu>
2020-07-21 15:20:12 -0700

    Added optional dependency for django prometheus

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'django-compressor',
         'django-pyscss',
         'django-prometheus',
+        'psycopg2-binary',
     ],
     license='Apache License, Version 2.0',
     description=('App to collect and tag interview data, artifacts, and stories for publishing using the Django Admin app.'),
