f37b0c49a7003e2b70fe84c1b29dd1341ff2df1a
Jim Laney <jlaney@uw.edu>
2021-05-07 08:19:36 -0700

    add license/copyright to src files

--- interview_db/views.py
+++ interview_db/views.py
@@ -1,3 +1,6 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.utils.decorators import method_decorator
