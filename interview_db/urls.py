4083cc89b9d7da5ad894b6eb87122ecacfd733c9
Charlon Palacay <charlon@gmail.com>
2022-07-05 14:43:03 -0700

    Adds new vue pages to django urls. Updates vue layout page title handling.

--- interview_db/urls.py
+++ interview_db/urls.py
@@ -28,14 +28,20 @@ if settings.DEBUG:
     ]
 
 urlpatterns += [
-    re_path(r'interviews.html', views.InterviewsView.as_view(), name="interviews"),
-    re_path(r'people.html', views.PeopleView.as_view(), name="people"),
-    re_path(r'topics.html', views.TopicsView.as_view(), name="topics"),
-    re_path(r'^interview/(?P<interview_id>\d+)/$',
-        views.interview, name='interview'),
-    re_path(r'^topic/(?P<code_id>\d+)/$',
-        views.code, name='code'),
-    re_path(r"^(customize|page2|page3)$", DefaultPageView.as_view()),
-    re_path(r'^$', TemplateView.as_view(template_name="vue.html"), name="home"),
+    re_path(
+        r"interviews.html", views.InterviewsView.as_view(), name="interviews"
+    ),
+    re_path(r"people.html", views.PeopleView.as_view(), name="people"),
+    re_path(r"topics.html", views.TopicsView.as_view(), name="topics"),
+    re_path(
+        r"^interview/(?P<interview_id>\d+)/$",
+        views.interview,
+        name="interview",
+    ),
+    re_path(r"^topic/(?P<code_id>\d+)/$", views.code, name="code"),
+    re_path(r"^(collections|students|about)$", DefaultPageView.as_view()),
+    re_path(
+        r"^$", TemplateView.as_view(template_name="vue.html"), name="home"
+    ),
     # re_path(r'^$', views.IndexView.as_view(), name="home"),
-] 
+]
