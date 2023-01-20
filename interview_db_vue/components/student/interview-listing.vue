a9d8c207be869e32f02c71f62e451428756d0244
Charlon Palacay <charlon@gmail.com>
2023-01-20 14:00:29 -0800

    Creates image directory for vue bundling. Removes axdd components lib.

--- interview_db_vue/components/student/interview-listing.vue
+++ interview_db_vue/components/student/interview-listing.vue
@@ -1,9 +1,19 @@
-// interview-listing.vue 
-// shown on full students page
+// interview-listing.vue // shown on full students page
 
 <template>
-  <button type="button" class="btn-card mt-5"
-    @click="$router.push({ name: 'Students', params: { id: studentInfo.id, singleStudent: JSON.stringify(studentInfo) } })">
+  <button
+    type="button"
+    class="btn-card mt-5"
+    @click="
+      $router.push({
+        name: 'Students',
+        params: {
+          id: studentInfo.id,
+          singleStudent: JSON.stringify(studentInfo),
+        },
+      })
+    "
+  >
     <p class="fs-6 text-end">{{ interviewDate }}</p>
     <div class="card-clickable">
       <div class="row p-0 m-0">
@@ -12,17 +22,22 @@
         </div>
 
         <div class="col-8 ps-4 m-0">
-          <h2 class="card-title fw-bold display-6 mb-2">{{ studentInfo.student.first_name }}</h2>
+          <h2 class="card-title fw-bold display-6 mb-2">
+            {{ studentInfo.student.first_name }}
+          </h2>
           <div class="row">
             <div class="col-11">
-              <p class="display-4 fs-6 mx-auto pb-4 border-bottom border-primary">
+              <p
+                class="display-4 fs-6 mx-auto pb-4 border-bottom border-primary"
+              >
                 <span v-if="studentInfo.standing">
                   {{ studentInfo.standing + ", studying" }}
                 </span>
-                <span v-else>
-                  Studying
-                </span>
-                <span v-for="major, index in studentInfo.major" :key="major.id">
+                <span v-else> Studying </span>
+                <span
+                  v-for="(major, index) in studentInfo.major"
+                  :key="major.id"
+                >
                   <span v-if="index != 0">, </span>
                   {{ major.full_title }}
                 </span>
@@ -57,8 +72,8 @@ export default {
   methods: {},
   computed: {
     interviewDate() {
-      return new Date(this.studentInfo.date).toLocaleDateString('en-US');
-    }
+      return new Date(this.studentInfo.date).toLocaleDateString("en-US");
+    },
   },
 };
 </script>
--- interview_db_vue/pages/about.vue
+++ interview_db_vue/pages/about.vue
@@ -11,9 +11,15 @@
       <div class="mx-auto p-5 mb-4">
         <div class="pt-5 ps-5 mx-auto mb-5">
           <h2 class="display-5 fw-bold mb-4">Who We Are</h2>
-          <p class="fs-5 mb-4">We collect the real stories of UW students and share them with others on campus.</p>
-          <p class="fs-5 mb-4">Our team operates out of the <b>Academic Experience Design and Delivery (AXDD)</b>
-            department at UW-IT.</p>
+          <p class="fs-5 mb-4">
+            We collect the real stories of UW students and share them with
+            others on campus.
+          </p>
+          <p class="fs-5 mb-4">
+            Our team operates out of the
+            <b>Academic Experience Design and Delivery (AXDD)</b> department at
+            UW-IT.
+          </p>
         </div>
 
         <div class="mx-auto pt-2 ps-5 mb-5">
@@ -25,7 +31,10 @@
 
         <div class="mx-auto pt-2 ps-5 mb-5">
           <h2 class="display-6 fs-2 fw-bold mb-4">Help Grow Our Collection</h2>
-          <p class="fs-5 mb-4">Schedule a 15-30 minute appointment with us to talk about your UW experience.</p>
+          <p class="fs-5 mb-4">
+            Schedule a 15-30 minute appointment with us to talk about your UW
+            experience.
+          </p>
           <a href="mailto:ux@uw.edu?subject=Husky Voices" title="Husky Voices">
             <button type="button" class="btn btn-secondary">
               Share Your Story >
@@ -34,71 +43,121 @@
         </div>
 
         <div class="mx-auto pt-2 ps-5 mb-5">
-          <h2 class="display-6 fs-2 fw-bold mb-4">Frequently Asked Questions (FAQ)</h2>
+          <h2 class="display-6 fs-2 fw-bold mb-4">
+            Frequently Asked Questions (FAQ)
+          </h2>
 
           <div class="mx-auto pt-2 mb-5">
             <div class="accordion" id="faq">
               <div class="accordion-item">
                 <p class="accordion-header fs-5" id="headingOne">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="false"
+                    aria-controls="collapseOne"
+                  >
                     How do you choose the students you interview?
                   </button>
                 </p>
-                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-                  data-bs-parent="#faq">
+                <div
+                  id="collapseOne"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#faq"
+                >
                   <div class="accordion-body">
-                    <p class="fs-5">We walk around busy spaces like the HUB Cafeteria or By George and look for students
-                      who look like they
-                      might have a little extra time to talk.</p>
+                    <p class="fs-5">
+                      We walk around busy spaces like the HUB Cafeteria or By
+                      George and look for students who look like they might have
+                      a little extra time to talk.
+                    </p>
                   </div>
                 </div>
               </div>
               <div class="accordion-item">
                 <p class="accordion-header fs-5" id="headingTwo">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                  >
                     Who conducts these interviews?
                   </button>
                 </p>
-                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                  data-bs-parent="#faq">
+                <div
+                  id="collapseTwo"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingTwo"
+                  data-bs-parent="#faq"
+                >
                   <div class="accordion-body">
-                    <p class="fs-5">Most interviews are conducted by student staff at UW-IT.</p>
+                    <p class="fs-5">
+                      Most interviews are conducted by student staff at UW-IT.
+                    </p>
                   </div>
                 </div>
               </div>
               <div class="accordion-item">
                 <p class="accordion-header fs-5" id="headingThree">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree"
+                    aria-expanded="false"
+                    aria-controls="collapseThree"
+                  >
                     How are the interviews transcribed?
                   </button>
                 </p>
-                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-                  data-bs-parent="#faq">
+                <div
+                  id="collapseThree"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingThree"
+                  data-bs-parent="#faq"
+                >
                   <div class="accordion-body">
-                    <p class="fs-5">We make edits for clarity and concision as well as re-organize the conversation to
-                      make student stories
-                      more readable. We are trained to take special care of conserving the original interviewee intent.
+                    <p class="fs-5">
+                      We make edits for clarity and concision as well as
+                      re-organize the conversation to make student stories more
+                      readable. We are trained to take special care of
+                      conserving the original interviewee intent.
                     </p>
                   </div>
                 </div>
               </div>
               <div class="accordion-item">
                 <p class="accordion-header fs-5" id="headingFour">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseFour"
+                    aria-expanded="false"
+                    aria-controls="collapseFour"
+                  >
                     How are the interviews transcribed?
                   </button>
                 </p>
-                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
-                  data-bs-parent="#faq">
+                <div
+                  id="collapseFour"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingFour"
+                  data-bs-parent="#faq"
+                >
                   <div class="accordion-body">
-                    <p class="fs-5">After each interview, we are obliged to give an additional form for the students to
-                      complete. This form clarifies whether they only want their information used for internal UW
-                      research
-                      or if they are willing to release their story to be shared.
+                    <p class="fs-5">
+                      After each interview, we are obliged to give an additional
+                      form for the students to complete. This form clarifies
+                      whether they only want their information used for internal
+                      UW research or if they are willing to release their story
+                      to be shared.
                     </p>
                   </div>
                 </div>
@@ -107,20 +166,17 @@
           </div>
         </div>
       </div>
-
     </template>
   </layout>
 </template>
 
 <script>
-import { Card } from "axdd-components";
 import Layout from "../layout.vue";
 
 export default {
   name: "PagesAbout",
   components: {
     layout: Layout,
-    "axdd-card": Card,
   },
   data() {
     return {
