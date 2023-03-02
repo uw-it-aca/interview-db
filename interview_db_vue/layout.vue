079f8354c1ecaa75018bd94a207aec1751fc3409
Delaney <delaney@AXDDs-MacBook-Pro-2.local>
2023-03-02 14:12:03 -0800

    updating text styles

--- interview_db_vue/layout.vue
+++ interview_db_vue/layout.vue
@@ -66,7 +66,7 @@
   <footer class="py-4 px-5 footer bg-purple text-white">
     <div class="row">
        <div class="col-6">
-        <img src="../images/washingtonlogo.svg" />
+        <!--<img src="../images/washingtonlogo.svg" /> -->
       </div>
       <div class="col-6">
         <p class="text-end justify-content-end">Copyright &copy; 2023 University of Washington</p>
