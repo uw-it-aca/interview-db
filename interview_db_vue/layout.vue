f94696108ac859695b9ab46283720b1e363146ef
Delaney <delaney@AXDDs-MacBook-Pro-2.local>
2023-03-08 13:26:12 -0800

    Fixing desktop header spacing

--- interview_db_vue/layout.vue
+++ interview_db_vue/layout.vue
@@ -1,8 +1,8 @@
 <template>
   <header>
     <nav class="bg-white p-2">
-      <div class="d-flex flex-column flex-md-row text-purple">
-        <div class="d-flex flex-row husky-header">
+      <div class="d-flex flex-md-row text-purple justify-content-between">
+        <div class="d-flex flex-row title-logo">
             <router-link class="nav-link text-purple" active-class="active" aria-current="page" to="/">
               <img src="./images/purplew.png" class="img-fluid justify-content-start"
                 style="object-fit:contain; width:100%; height:50px" alt="Husky W Logo">
@@ -32,7 +32,7 @@
             </h2>
           </button> -->
         <!-- </div> -->
-        <ul class="nav mt-2 mx-auto p-2 col-6 justify-content-end nav-list">
+        <ul class="nav mt-2 p-2 col-6 justify-content-end nav-list">
           <li class="nav-item">
             <h6>
               <router-link class="nav-link text-purple" active-class="active" aria-current="page" to="/students">Student
