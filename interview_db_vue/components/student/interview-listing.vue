e32124627a9bed49f471139d3fa7b4b45784996e
Delaney <delaney@AXDDs-MacBook-Pro-2.local>
2023-04-18 10:52:28 -0700

    Fixing student cards

--- interview_db_vue/components/student/interview-listing.vue
+++ interview_db_vue/components/student/interview-listing.vue
@@ -15,24 +15,38 @@
       })
     "
   >
-  <div>
     <div class="d-flex card-clickable">
       <div class="row p-0 m-0 card-img-row align-items-end">
-        <!--<div class="row card-date-row">
+        <div class="row card-date-row">
           <p class="fs-6 text-end">{{ interviewDate }}</p>
-        </div> -->
+        </div>
         <div class="col-4 mx-auto ps-4">
           <img src="../../css/blossom.png" class="listing-img" />
         </div>
 
         <div class="col-8 ps-4 m-0 align-items-baseline">
-          <div class="row card-date-row align-self-start">
+          <div class="row card-date-row">
             <p class="fs-6 text-end">{{ interviewDate }}</p>
-          </div>
           </div>
           <h2 class="card-title fw-bold text-purple display-6 mb-2">
             {{ studentInfo.student.first_name }}
           </h2>
+          <p
+                class="display-4 fs-6 mx-auto pb-4 border-bottom border-primary"
+              >
+                <span v-if="studentInfo.standing">
+                  {{ studentInfo.standing + ", studying" }}
+                </span>
+                <span v-else> Studying </span>
+                <span
+                  v-for="(major, index) in studentInfo.major"
+                  :key="major.id"
+                >
+                  <span v-if="index != 0">, </span>
+                  {{ major.full_title }}
+                </span>
+              </p>
+        <!--</div>
           <div class="row">
             <div class="col-11">
               <p
@@ -52,9 +66,9 @@
               </p>
             </div>
           </div>
-        </div>
+        </div> -->
 
-        <div class="row card-text p-4">
+        <div class="card-text p-4">
           <p class="display-6 fs-5">"{{ studentInfo.pull_quote }}"</p>
         </div>
         <div class="d-flex justify-content-end">
