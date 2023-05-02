b907373f01319679357d78c296df26e9cdbc2edf
Delaney <delaney@AXDDs-MacBook-Pro-2.local>
2023-05-02 16:56:02 -0700

    Trying to load in data for carousel

--- interview_db_vue/pages/students.vue
+++ interview_db_vue/pages/students.vue
@@ -59,7 +59,7 @@ export default {
     singleStudent: {
       type: String,
       required: false,
-    }
+    },
   },
   data() {
     return {
--- interview_db_vue/router/index.js
+++ interview_db_vue/router/index.js
@@ -11,6 +11,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    props: true,
     component: Home,
   },
   {
