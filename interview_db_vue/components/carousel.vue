d769efd421890acaf2d3711251db7e2ee5795c95
Delaney <delaney@AXDDs-MacBook-Pro-2.local>
2023-05-11 13:35:14 -0700

    Styling carousel cards

--- interview_db_vue/components/carousel.vue
+++ interview_db_vue/components/carousel.vue
@@ -48,31 +48,33 @@
 
     <div class="d-flex card-clickable">
       <div class="row p-0 m-x-0">
-        <div class="col-md-5 col-sm-6 row-xs mx-auto ps-4 img-div shift-up">
-          <img src="../../css/quad.png" class="listing-img" />
-        </div>
+        <div class="row p-0 m-0 border-bottom border-primary">
+          <div class="col-md-5 col-sm-6 row-xs ps-4 img-div shift-up">
+            <img src="../css/quad.png" class="listing-img" />
+          </div>
 
-        <div class="col-md-7 col-sm-6 ps-4 m-0">
-          <div class="row">
-            <p class="fs-6 text-end">{{ interviewDate }}</p>
-            <h2 class="card-title fw-bold text-purple display-6 mb-2">
-              {{ studentInfo.student.first_name }}
-            </h2>
-            <p class="display-4 fs-6 mx-auto pb-4 border-bottom border-primary">
-              <span v-if="studentInfo.standing">
-                {{ studentInfo.standing + ", studying" }}
-              </span>
-              <span v-else> Studying </span>
-              <span v-for="(major, index) in studentInfo.major" :key="major.id">
-                <span v-if="index != 0">, </span>
-                {{ major.full_title }}
-              </span>
-            </p>
+          <div class="col-md-7 col-sm-6 ps-4 m-0">
+            <div class="row">
+              <p class="fs-6 text-end">{{ interviewDate }}</p>
+              <h2 class="card-title fw-bold text-purple display-6 mb-2">
+                {{ studentInfo.student.first_name }}
+              </h2>
+              <p class="display-4 fs-6 mx-auto pb-4">
+                <span v-if="studentInfo.standing">
+                  {{ studentInfo.standing + ", studying" }}
+                </span>
+                <span v-else> Studying </span>
+                <span v-for="(major, index) in studentInfo.major" :key="major.id">
+                  <span v-if="index != 0">, </span>
+                  {{ major.full_title }}
+                </span>
+              </p>
+            </div>
           </div>
         </div>
 
-        <div class="card-text px-4">
-          <p class="display-6 fs-5">"{{ studentInfo.pull_quote }}"</p>
+        <div class="card-text pt-2 px-4">
+          <p class="display-6 fs-6">"{{ studentInfo.pull_quote }}"</p>
         </div>
         <div class="d-flex justify-content-end">
             <u class="text-purple" style="display:inline;">Read More</u>
