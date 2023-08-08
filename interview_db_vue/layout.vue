1485be61f3435930075ea6655a65a121748ded3b
Nancy Huang <nhuang1@uw.edu>
2023-08-08 14:47:13 -0700

    set mq breakpoints; center title on mobile

--- interview_db_vue/layout.vue
+++ interview_db_vue/layout.vue
@@ -3,37 +3,20 @@
     <nav class="bg-white p-2">
       <div class="d-flex flex-md-row text-purple justify-content-between">
         <div class="d-flex flex-row title-logo">
+          <router-link class="nav-link text-purple" active-class="active" aria-current="page" to="/">
+            <img src="./images/purplew.png" class="img-fluid justify-content-start"
+              style="object-fit:contain; width:100%; height:50px" alt="Husky W Logo">
+          </router-link>
+          <h2
+            :class="[mq.mobile ? 'fs-3 d-flex justify-content-center fw-bold' : 'fs-3 d-flex justify-content-start fw-bold',]">
             <router-link class="nav-link text-purple" active-class="active" aria-current="page" to="/">
-              <img src="./images/purplew.png" class="img-fluid justify-content-start"
-                style="object-fit:contain; width:100%; height:50px" alt="Husky W Logo">
+              Husky Voices
             </router-link>
-            <h2 class="fs-3 d-flex justify-content-start fw-bold">
-              <router-link class="nav-link text-purple" active-class="active" aria-current="page" to="/">
-                Husky Voices
-              </router-link>
-            </h2>
-          <router-link active-class="active" aria-current="page" to="/menu">
+          </h2>
+          <router-link v-if="mq.current === 'mobile'" active-class="active" aria-current="page" to="/menu">
             <i class="bi bi-list hamburger-menu"></i>
           </router-link>
         </div>
-<!-- 
-        <router-link class="nav-link text-purple" active-class="active" aria-current="page" to="/">
-          <div><img src="./images/purplew.png" class="justify-content-start"
-              style="object-fit:contain; width:100%; height:50px; position:absolute" alt="Husky W Logo"></div>
-          <div>
-            <h2 class="fs-3 fw-bold">
-              Husky Voices
-            </h2>
-          </div>
-        </router-link> -->
-
-        <!-- <button @click="$router.push({ name: 'Home' })" class="border-0 bg-white">
-            <img src="./images/purplew.png" style="object-fit:contain; width:100%; height:50px"
-              alt="Husky W Logo">
-            <h2 class="fs-3 fw-bold text-purple">Husky Voices
-            </h2>
-          </button> -->
-        <!-- </div> -->
         <ul class="nav mt-2 p-2 col-6 justify-content-end nav-list">
           <li class="nav-item">
             <h6>
@@ -69,7 +52,7 @@
 
   <footer class="py-4 px-5 footer bg-purple text-white">
     <div class="row">
-       <div class="col-md-6 col-12 footer-img d-flex">
+      <div class="col-md-6 col-12 footer-img d-flex">
         <img src="./images/washingtonlogo.svg" />
       </div>
       <div class="col-md-6 col-12 align-items-center footer-copyright d-flex">
--- interview_db_vue/mobilelayout.vue
+++ interview_db_vue/mobilelayout.vue
@@ -1,54 +1,42 @@
 <template>
-    <nav class="bg-white p-2">
-        <!--<div class="d-flex flex-md-row text-purple justify-content-between">
-          <div class="d-flex flex-row title-logo">
-              <router-link class="nav-link text-purple" active-class="active" aria-current="page" to="/">
-                <img src="./images/purplew.png" class="img-fluid justify-content-start"
-                  style="object-fit:contain; width:100%; height:50px" alt="Husky W Logo">
-              </router-link>
-              <h2 class="fs-3 d-flex justify-content-start fw-bold">
-                <router-link class="nav-link text-purple" active-class="active" aria-current="page" to="/">
-                  Husky Voices
-                </router-link>
-              </h2>
-          </div> -->
-        <div class="row mt-2 px-4">
-          <button type="button" class="btn-close justify-content-end" aria-label="Close" @click="$router.go(-1)"></button>
-        </div>
-        <ul class="nav mt-2 p-2 flex-column justify-content-start">
-            <li class="nav-item">
-                <h6>
-                    <router-link class="nav-link text-purple row" active-class="active" aria-current="page" to="/">
-                        Home
-                    </router-link>
-                </h6>
-            </li>
-            <li class="nav-item">
-              <h6>
-                <router-link class="nav-link text-purple row" active-class="active" aria-current="page" to="/students">Student
-                  Stories
-                </router-link>
-              </h6>
-            </li>
-            <li class="nav-item">
-              <h6>
-                <router-link class="nav-link text-purple row" active-class="active" aria-current="page"
-                  to="/collections">Collections
-                </router-link>
-              </h6>
-            </li>
-            <li class="nav-item">
-              <h6>
-                <router-link class="nav-link text-purple row" active-class="active" aria-current="page" to="/about">About
-                  Us</router-link>
-              </h6>
-            </li>
-          </ul>
-      </nav>
-  </template>
+  <nav class="bg-white p-2">
+    <div class="row mt-2 px-4">
+      <button type="button" class="btn-close justify-content-end" aria-label="Close" @click="$router.go(-1)"></button>
+    </div>
+    <ul class="nav mt-2 p-2 flex-column justify-content-start">
+      <li class="nav-item">
+        <h6>
+          <router-link class="nav-link text-purple row" active-class="active" aria-current="page" to="/">
+            Home
+          </router-link>
+        </h6>
+      </li>
+      <li class="nav-item">
+        <h6>
+          <router-link class="nav-link text-purple row" active-class="active" aria-current="page" to="/students">Student
+            Stories
+          </router-link>
+        </h6>
+      </li>
+      <li class="nav-item">
+        <h6>
+          <router-link class="nav-link text-purple row" active-class="active" aria-current="page"
+            to="/collections">Collections
+          </router-link>
+        </h6>
+      </li>
+      <li class="nav-item">
+        <h6>
+          <router-link class="nav-link text-purple row" active-class="active" aria-current="page" to="/about">About
+            Us</router-link>
+        </h6>
+      </li>
+    </ul>
+  </nav>
+</template>
   
 <script>
 export default {
-    name: "LayoutMobile",
-  };
+  name: "LayoutMobile",
+};
 </script>
