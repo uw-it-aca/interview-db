6af6d99069454125f8c5296f5d9fd187775f31ab
Nancy Huang <nhuang1@uw.edu>
2023-08-17 13:38:08 -0700

    use conditional class binding for interview-listing

--- interview_db_vue/components/student/interview-listing.vue
+++ interview_db_vue/components/student/interview-listing.vue
@@ -20,20 +20,10 @@
         <div class="col-md-8 col-sm-7 p-4 m-0">
           <div class="row">
             <p v-if=!carousel class="fs-6 text-end">{{ interviewDate }}</p>
-            <h2 v-if=carousel class="card-title fw-bold text-purple display-4 fs-3 mb-2">
+            <h2 class="card-title fw-bold text-purple" :class="carousel ? 'display-4 fs-3 mb-2' : 'display-6 mb-2'">
               {{ interviewInfo.student.first_name }}
             </h2>
-            <h2 v-else class="card-title fw-bold text-purple display-6 mb-2">
-              {{ interviewInfo.student.first_name }}
-            </h2>
-            <p v-if=carousel class="pb-4 border-bottom border-primary">
-              <span v-if="interviewInfo.standing">
-                {{ interviewInfo.standing + ", studying" }}
-              </span>
-              <span v-else> Studying </span>
-              {{ interviewInfo.declared_major }}
-            </p>
-            <p v-else class="display-4 fs-5 pb-4 border-bottom border-primary">
+            <p class="pb-4 border-bottom border-primary" :class="carousel ? '' : 'display-4 fs-5'">
               <span v-if="interviewInfo.standing">
                 {{ interviewInfo.standing + ", studying" }}
               </span>
@@ -44,14 +34,12 @@
         </div>
 
         <div class="card-text px-4">
-          <p v-if=carousel class="lh-base">"{{ interviewInfo.pull_quote }}"</p>
-          <p v-else-if=story class="display-4 fs-6 lh-base">"{{ story }}"</p>
-          <p v-else class="display-4 fs-6 lh-base">"{{ interviewInfo.pull_quote }}"</p>
+          <p v-if=story class="lh-base">"{{ story }}"</p>
+          <p v-else class="lh-base">"{{ interviewInfo.pull_quote }}"</p>
         </div>
 
         <div class="d-flex justify-content-end">
-          <u v-if=carousel class="text-purple" style="display:inline;">Read More</u>
-          <u v-else class="display-4 fs-6 text-purple" style="display:inline;">Read More</u>
+          <u class="text-purple" style="display:inline;">Read More</u>
           <p>&nbsp;</p>
           <i class="bi bi-chevron-right"></i>
         </div>
