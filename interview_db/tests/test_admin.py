ffdd42eed87a7df4d7f96f58664198972f295a85
Nancy <nhuang1@uw.edu>
2023-08-28 12:20:49 -0700

    add admin and redirect tests

---
+++ interview_db/tests/test_admin.py
@@ -0,0 +1,29 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from django.test import TestCase
+from django.urls import reverse
+from interview_db.models import *
+from django.contrib.admin.sites import AdminSite
+from interview_db.admin import *
+from unittest.mock import Mock
+
+
+class AdminTest(TestCase):
+    fixtures = ["collections.json"]
+
+    def setUp(self):
+        self.site = AdminSite()
+
+    def test_redirect(self):
+        """
+        Test navigating to admin endpoint redirects to SAML login page
+        """
+        url = reverse("admin:index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["Location"],
+                         "/admin/login/?next=/admin/")
+
+    # def test_collection(self):
+    #     collections_admin = CollectionAdmin(Collection, self.site)
--- interview_db/tests/views/test_redirect.py
+++ interview_db/tests/views/test_redirect.py
@@ -4,17 +4,44 @@
 from django.test import TestCase
 from django.urls import reverse
 from interview_db.models import *
-import json
 
 
 class LoginRedirect(TestCase):
+    def setUp(self):
+        joe = Student.objects.create(
+            first_name="Joe",
+            last_name="Average",
+            follow_up_consent=False,
+        )
+        interview = Interview.objects.create(
+            student=joe,
+            date="2021-09-29",
+            signed_release_form=True,
+            image_is_not_identifying=True,
+            intended_major=True,
+            no_identifying_photo=True,
+            no_real_name=False,
+            no_publishing_stories=False,
+            other_publishing_restrictions=False
+        )
+        self.interview = interview
 
-    def test_redirect(self):
+    def test_redirect_home(self):
         """
         Test upon loading home page redirects to SAML login page
         """
         url = reverse("interview_db:home")
         response = self.client.get(url)
-        print(response)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, "/saml/login?next=/")
+        self.assertEqual(response["Location"], "/saml/login?next=/")
+
+    def test_redirect_interview(self):
+        """
+        Test navigating to student inteview page redirects to SAML login page
+        """
+        url = reverse("interview_db:student-detail",
+                      kwargs={"id": self.interview.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["Location"],
+                         "/saml/login?next=/api/students/1/")
