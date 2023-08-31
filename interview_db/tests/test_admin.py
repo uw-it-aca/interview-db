aa18404f247bd414e8c25fa3332ec1a3663b8c13
Nancy Huang <nhuang1@uw.edu>
2023-08-31 15:08:16 -0700

    refactor admin tests and test admin getters

--- interview_db/tests/test_admin.py
+++ interview_db/tests/test_admin.py
@@ -14,7 +14,7 @@ admin_group = settings.INTERVIEW_DB_AUTHZ_GROUPS['admin']
 
 
 class MockRequest:
-    pass
+    session = {'samlUserdata': {'isMemberOf': 'u_test_admin'}}
 
 
 class AdminTest(TestCase):
@@ -23,14 +23,42 @@ class AdminTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
             username="javerage", password="pass")
+        self.request = MockRequest()
+        self.request.user = self.user
         self.site = AdminSite()
-        self.collection = Collection.objects.create(
-            topic="Moving Forward")
-        self.collection.code = Code.objects.get_or_create(
-            code="Finding Community")[0]
-        self.collection.subcode = SubCode.objects.get_or_create(
-            subcode="Context")[0]
-        self.collection.save()
+
+        self.student = Student.objects.create(
+            first_name="Joe",
+            last_name="Average",
+            follow_up_consent=False,
+        )
+
+        self.interview = Interview.objects.create(
+            student=self.student,
+            date="2021-09-29",
+            signed_release_form=True,
+            image_is_not_identifying=True,
+            intended_major=True,
+            no_identifying_photo=True,
+            no_real_name=False,
+            no_publishing_stories=False,
+            other_publishing_restrictions=False
+        )
+
+        self.story = Story.objects.create(
+            interview=self.interview,
+            story="This is Joe's first story about Learning!",
+            story_order_position=1,
+        )
+
+        self.major_admin = MajorAdmin(Major, self.site)
+        self.student_admin = StudentAdmin(Student, self.site)
+        self.interview_admin = InterviewAdmin(Interview, self.site)
+        self.coding_admin = CodingAdmin(Coding, self.site)
+        self.subcode_admin = SubCodeAdmin(SubCode, self.site)
+        self.code_admin = CodeAdmin(Code, self.site)
+        self.story_admin = StoryAdmin(Story, self.site)
+        self.collection_admin = CollectionAdmin(Collection, self.site)
 
     def test_redirect(self):
         """
@@ -42,86 +70,111 @@ class AdminTest(TestCase):
         self.assertEqual(response["Location"],
                          "/admin/login/?next=/admin/")
 
+    def test_interview_followup(self):
+        self.assertEqual(self.interview_admin.get_followup(self.interview),
+                         False)
+
+    def test_story_methods(self):
+        self.assertEqual(self.story_admin.get_first_name(self.story),
+                         "Joe")
+        self.assertEqual(self.story_admin.get_last_name(self.story),
+                         "Average")
+        self.assertEqual(self.story_admin.get_date(self.story),
+                         "2021-09-29")
+
     def test_permissions_major(self):
-        admin = MajorAdmin(Major, self.site)
-        request = MockRequest()
-        request.user = self.user
-        self.assertEqual(admin.has_view_permission(request), False)
-        self.assertEqual(admin.has_add_permission(request), False)
-        self.assertEqual(admin.has_change_permission(request), False)
-        self.assertEqual(admin.has_delete_permission(request), False)
-        self.assertEqual(admin.has_module_permission(request), False)
+        self.assertEqual(self.major_admin.has_view_permission(self.request),
+                         False)
+        self.assertEqual(self.major_admin.has_add_permission(self.request),
+                         False)
+        self.assertEqual(self.major_admin.has_change_permission(self.request),
+                         False)
+        self.assertEqual(self.major_admin.has_delete_permission(self.request),
+                         False)
+        self.assertEqual(self.major_admin.has_module_permission(self.request),
+                         False)
 
     def test_permissions_student(self):
-        admin = StudentAdmin(Student, self.site)
-        request = MockRequest()
-        request.session = {'samlUserdata': {'isMemberOf': 'u_test_admin'}}
-        request.user = self.user
-        self.assertEqual(admin.has_view_permission(request), True)
-        self.assertEqual(admin.has_add_permission(request), True)
-        self.assertEqual(admin.has_change_permission(request), True)
-        self.assertEqual(admin.has_delete_permission(request), True)
-        self.assertEqual(admin.has_module_permission(request), True)
+        self.assertEqual(self.student_admin.has_view_permission(
+            self.request), True)
+        self.assertEqual(self.student_admin.has_add_permission(
+            self.request), True)
+        self.assertEqual(self.student_admin.has_change_permission(
+            self.request), True)
+        self.assertEqual(self.student_admin.has_delete_permission(
+            self.request), True)
+        self.assertEqual(self.student_admin.has_module_permission(
+            self.request), True)
 
     def test_permissions_interview(self):
-        admin = InterviewAdmin(Interview, self.site)
-        request = MockRequest()
-        request.session = {'samlUserdata': {'isMemberOf': 'u_test_admin'}}
-        request.user = self.user
-        self.assertEqual(admin.has_view_permission(request), True)
-        self.assertEqual(admin.has_add_permission(request), True)
-        self.assertEqual(admin.has_change_permission(request), True)
-        self.assertEqual(admin.has_delete_permission(request), True)
-        self.assertEqual(admin.has_module_permission(request), True)
+        self.assertEqual(self.interview_admin.has_view_permission(
+            self.request), True)
+        self.assertEqual(self.interview_admin.has_add_permission(
+            self.request), True)
+        self.assertEqual(self.interview_admin.has_change_permission(
+            self.request), True)
+        self.assertEqual(self.interview_admin.has_delete_permission(
+            self.request), True)
+        self.assertEqual(self.interview_admin.has_module_permission(
+            self.request), True)
 
     def test_permissions_coding(self):
-        admin = CodingAdmin(Coding, self.site)
-        request = MockRequest()
-        request.user = self.user
-        self.assertEqual(admin.has_view_permission(request), False)
-        self.assertEqual(admin.has_add_permission(request), False)
-        self.assertEqual(admin.has_change_permission(request), False)
-        self.assertEqual(admin.has_delete_permission(request), False)
-        self.assertEqual(admin.has_module_permission(request), False)
+        self.assertEqual(self.coding_admin.has_view_permission(
+            self.request), False)
+        self.assertEqual(self.coding_admin.has_add_permission(
+            self.request), False)
+        self.assertEqual(self.coding_admin.has_change_permission(
+            self.request), False)
+        self.assertEqual(self.coding_admin.has_delete_permission(
+            self.request), False)
+        self.assertEqual(self.coding_admin.has_module_permission(
+            self.request), False)
 
     def test_permissions_subcode(self):
-        admin = SubCodeAdmin(SubCode, self.site)
-        request = MockRequest()
-        request.user = self.user
-        self.assertEqual(admin.has_view_permission(request), False)
-        self.assertEqual(admin.has_add_permission(request), False)
-        self.assertEqual(admin.has_change_permission(request), False)
-        self.assertEqual(admin.has_delete_permission(request), False)
-        self.assertEqual(admin.has_module_permission(request), False)
+        self.assertEqual(self.subcode_admin.has_view_permission(
+            self.request), False)
+        self.assertEqual(self.subcode_admin.has_add_permission(
+            self.request), False)
+        self.assertEqual(self.subcode_admin.has_change_permission(
+            self.request), False)
+        self.assertEqual(self.subcode_admin.has_delete_permission(
+            self.request), False)
+        self.assertEqual(self.subcode_admin.has_module_permission(
+            self.request), False)
 
     def test_permissions_code(self):
-        admin = CodeAdmin(Code, self.site)
-        request = MockRequest()
-        request.user = self.user
-        self.assertEqual(admin.has_view_permission(request), False)
-        self.assertEqual(admin.has_add_permission(request), False)
-        self.assertEqual(admin.has_change_permission(request), False)
-        self.assertEqual(admin.has_delete_permission(request), False)
-        self.assertEqual(admin.has_module_permission(request), False)
+        self.assertEqual(self.code_admin.has_view_permission(
+            self.request), False)
+        self.assertEqual(self.code_admin.has_add_permission(
+            self.request), False)
+        self.assertEqual(self.code_admin.has_change_permission(
+            self.request), False)
+        self.assertEqual(self.code_admin.has_delete_permission(
+            self.request), False)
+        self.assertEqual(self.code_admin.has_module_permission(
+            self.request), False)
 
     def test_permissions_story(self):
-        admin = StoryAdmin(Story, self.site)
-        request = MockRequest()
-        request.session = {'samlUserdata': {'isMemberOf': 'u_test_admin'}}
-        request.user = self.user
-        self.assertEqual(admin.has_view_permission(request), True)
-        self.assertEqual(admin.has_add_permission(request), True)
-        self.assertEqual(admin.has_change_permission(request), True)
-        self.assertEqual(admin.has_delete_permission(request), True)
-        self.assertEqual(admin.has_module_permission(request), True)
+        self.assertEqual(self.story_admin.has_view_permission(
+            self.request), True)
+        self.assertEqual(self.story_admin.has_add_permission(
+            self.request), True)
+        self.assertEqual(self.story_admin.has_change_permission(
+            self.request), True)
+        self.assertEqual(self.story_admin.has_delete_permission(
+            self.request), True)
+        self.assertEqual(self.story_admin.has_module_permission(
+            self.request), True)
 
     def test_permission_collection(self):
-        admin = CollectionAdmin(Collection, self.site)
-        request = MockRequest()
-        request.user = self.user
-        self.assertEqual(admin.has_view_permission(request), False)
-        self.assertEqual(admin.has_add_permission(request), False)
-        self.assertEqual(admin.has_change_permission(request), False)
-        self.assertEqual(admin.has_delete_permission(request), False)
-        self.assertEqual(admin.has_module_permission(request),
+        self.assertEqual(self.collection_admin.has_view_permission(
+            self.request), False)
+        self.assertEqual(self.collection_admin.has_add_permission(
+            self.request), False)
+        self.assertEqual(self.collection_admin.has_change_permission(
+            self.request), False)
+        self.assertEqual(self.collection_admin.has_delete_permission(
+            self.request), False)
+        self.assertEqual(self.collection_admin.has_module_permission(
+            self.request),
                          False)
