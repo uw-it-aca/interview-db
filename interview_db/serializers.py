66db96af28f409b8b30c629b485d5dcc3edb47e4
Nancy Huang <nhuang1@uw.edu>
2024-04-29 16:46:19 -0700

    delete interviewcollection serializer

--- interview_db/serializers.py
+++ interview_db/serializers.py
@@ -62,56 +62,6 @@ class InterviewIdSerializer(serializers.ModelSerializer):
         fields = ['id']
 
 
-class InterviewCollectionSerializer(serializers.ModelSerializer):
-    student = StudentSerializer(read_only=True)
-    major = MajorSerializer(many=True, read_only=True)
-    image = serializers.ImageField(max_length=None, use_url=True,
-                                   allow_empty_file=True, required=False)
-    standing = serializers.CharField(source='get_standing_display')
-    # collections = serializers.SerializerMethodField()
-
-    def get_collections(self, Interview):
-        interview = Story.objects.filter(interview__id=Interview.id)
-        queryset = set()
-        curr_codes = set()
-        # for story in interview:
-        #     for code in story.code.all():
-        #         curr_codes.add(code)
-        #     for code in story.subcode.all():
-        #         curr_codes.add(code)
-
-        # for topic in Collection.objects.all():
-        #     for code in topic.codes.all():
-        #         if code in curr_codes:
-        #             queryset.add(topic)
-        #             continue
-        #     for code in topic.subcodes.all():
-        #         if code in curr_codes:
-        #             queryset.add(topic)
-        #             continue
-
-        # serializer = CollectionFilterSerializer(queryset, many=True)
-        # return serializer.data
-
-    class Meta:
-        model = Interview
-        fields = ['id',
-                  'student',
-                  'major',
-                  'date',
-                  'signed_release_form',
-                  'pull_quote',
-                  'declared_major',
-                  'image',
-                  'image_is_not_identifying',
-                  'image_alt_text',
-                  'standing',
-                  'no_identifying_photo',
-                  'no_real_name',
-                  'no_publishing_stories',
-                  'other_publishing_restrictions']
-
-
 class StorySerializer(serializers.ModelSerializer):
     interview = InterviewSerializer(read_only=True)
     code = CodeSerializer(many=True, read_only=True)
